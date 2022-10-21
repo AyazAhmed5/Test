cf85b6c05777da39db50b3a75109a8dc8f53199a
Ayaz Ahmed <ahmedayaz525@gmial.comgit config --global user.name git config --global user.email ahmedayaz525@gmial.com>
2022-10-21 15:47:38 +0500

    Hello

--- src/store/counter/counter-action.js
+++
@@ -1,9 +0,0 @@
-import { COUNTER_ACTION_TYPES } from "./counter-types";
-import { createAction } from "../../utils/reducer-utils";
-
-export const INCREMENT = (temp) => {
-  createAction(COUNTER_ACTION_TYPES.COUNTER_INCREMENT, temp);
-};
-export const DECREMENT = (temp) => {
-  createAction(COUNTER_ACTION_TYPES.COUNTER_DECREMENT, temp);
-};
--- src/store/root-reducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import { counterReducer } from "../store/counter/counter-reducer";
-
-export const rootReducer = () => {
-  combineReducers({
-    counter: counterReducer,
-  });
-};
